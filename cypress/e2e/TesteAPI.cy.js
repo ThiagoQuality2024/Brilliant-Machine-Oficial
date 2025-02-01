cbcafb0b6a02d4d505a08ad7caeed5ef3472028d
Thiago <Thiago@teste.com>
2025-02-01 18:36:09 -0300

    commit 1

--- cypress/e2e/TesteAPI.cy.js
+++ cypress/e2e/TesteAPI.cy.js
@@ -15,6 +15,8 @@ describe('Teste API', () => {
       // 5 e 6. Verificar os objetos com rating maior do que 4 e exibir no console da Web. F12->CONSOLE
       TesteApi.ExibicaoDosElementosNoConsoleMaiorQue4();
       cy.wait(5000);
+      //thiago
+      //atualização feita na branch thiago
     });
   });
 });
